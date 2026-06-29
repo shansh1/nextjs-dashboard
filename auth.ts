caed432a84d9db13a8581f053d445f9ac81d0213
shansh1 <yushuchen9v@gmail.com>
2026-06-29 13:51:41 +0800

    14.1 add Authentication

---
+++ auth.ts
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from 'next-auth/providers/credentials';
+import z from "zod";
+import type { User } from "./app/lib/definitions";
+import bcrypt from 'bcrypt'
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+    return user[0];
+  } catch (e) {
+    console.error("Failed to fetch user: ", e);
+    throw new Error('Failed to fetch user.');
+  }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [Credentials({
+    async authorize(credentials) {
+      const parseCredentials = z.object({ email: z.string(), password: z.string().min(6) }).safeParse(credentials);
+      if (parseCredentials.success) {
+        const { email, password } = parseCredentials.data;
+        const user = await getUser(email);
+        if (!user) return null;
+
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordsMatch) return user;
+      }
+      console.log('Invalid credentials');
+      return null;
+    }
+  })],
+});
